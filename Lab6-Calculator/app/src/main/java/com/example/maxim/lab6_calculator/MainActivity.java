d1b2bdacc9b898a206e08a8e551f7a6d29b9bf99
DrKrutus <thekrutus@gmail.com>
2016-11-13 17:28:22 +0200

    Only buttons

--- Lab6-Calculator/app/src/main/java/com/example/maxim/lab6_calculator/MainActivity.java
+++ Lab6-Calculator/app/src/main/java/com/example/maxim/lab6_calculator/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.maxim.lab6_calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,3 +13,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 }
+
+    Button b1, b2, b3, b4, b5, b6, b7, b8; /*Указатель кнопок*/
+    EditText e1, e2, e3; /*Указатель текста*/
+        e1 = (EditText) findViewById(R.id.editText1);/*Указатель текстового поля*/
+        b1 = (Button) findViewById(R.id.button1);/*Указатель кнопки*/
+                String s = e1.getText().toString();/*Читать из поля ввода*/
+                   e1.setText(s);/*Писать в поле*/
+                     int X=Integer.parseInt(e1.getText().toString());/*Преобразуем содержимое строки в число*/
+                        int Res = X%Y;/*остаток от деления*/
+                           int Res = X^Y;/*Сумма по модулю два*/
+                               String s = Integer.toBinaryString(X);/*перевод в  двоичный*/
